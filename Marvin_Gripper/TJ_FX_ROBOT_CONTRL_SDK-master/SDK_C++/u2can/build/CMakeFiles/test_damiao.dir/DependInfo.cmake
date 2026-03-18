a1efb73fe975226f750c443b4774e3b4b2a67de5
Li Haoran <18813091982@163.com>
2026-03-18 11:00:49 +0800

    Add files via upload

---
+++ Marvin_Gripper/TJ_FX_ROBOT_CONTRL_SDK-master/SDK_C++/u2can/build/CMakeFiles/test_damiao.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/hanwen/KernalMind-OmniGripper-Control-Examples-main/Marvin_Gripper/TJ_FX_ROBOT_CONTRL_SDK-master/SDK_C++/u2can/src/test_damiao.cpp" "CMakeFiles/test_damiao.dir/src/test_damiao.cpp.o" "gcc" "CMakeFiles/test_damiao.dir/src/test_damiao.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
